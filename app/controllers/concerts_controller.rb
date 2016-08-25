31958a191923f59ffe59bd298e000c7df5a802d4
Justin Carothers <justincarothers@gmail.com>
2016-08-25 14:15:43 -0700

    added form to render to create new shows

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,22 @@
 class ConcertsController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@concert = Concert.new #create instance of concert
+	end
+
+	def create
+		@concert = Concert.new(params.require(:concert).permit(:title, :description, :band))
+	end
+
+
+	private 
+
+	def concert_params
+		params.require(:concert).permit(:title, :description, :director)
+	end
+
+
 end
